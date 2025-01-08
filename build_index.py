003c05221d5e2fc7c757bb44bec4d7bde38389bd
Steve Göring <steve.goering@tu-ilmenau.de>
2025-01-08 18:46:58 +0100

    add query part and restructure code

--- build_index.py
+++ build_index.py
@@ -3,39 +3,12 @@ import argparse
 import sys
 import os
 import glob
-import multiprocessing
+import json
+#import multiprocessing
 
-import torch
-from PIL import Image
-import open_clip
+from tqdm import tqdm
 
-
-class CLIP:
-    def __init__(self):
-        self.tokenizer = open_clip.get_tokenizer('ViT-B-32')
-        self.model, _, self.preprocess = open_clip.create_model_and_transforms('ViT-B-32', pretrained='laion2b_s34b_b79k')
-        self.model.eval()  # model in train mode by default, impacts some models with BatchNorm or stochastic depth active
-        print(open_clip.list_pretrained())
-
-    def parse_text(self, text):
-        text = self.tokenizer([text])
-        text_features = self.model.encode_text(text)
-
-        text_features /= text_features.norm(dim=-1, keepdim=True)
-
-        return text_features
-
-    def calc_image_embedding_text_embedding_similarity(self, image_embedding, text_embedding):
-        text_probs = (100.0 * image_embedding @ text_embedding.T).softmax(dim=-1)
-        return text_probs
-
-    def get_image_features(self, image_path):
-        image = self.preprocess(Image.open(image_path)).unsqueeze(0)
-        #with torch.no_grad(), torch.cuda.amp.autocast():
-        image_features = self.model.encode_image(image)
-        image_features /= image_features.norm(dim=-1, keepdim=True)
-
-        return image_features.tolist()
+from clip_utils import CLIP
 
 
 def main(_):
@@ -45,10 +18,10 @@ def main(_):
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("image_folder", type=str, help="images to be processed")
     parser.add_argument("--index_folder", type=str, default="index", help="folder for storing the clip features/index")
-    parser.add_argument('--cpu_count', type=int, default=multiprocessing.cpu_count(), help='thread/cpu count')
+    #parser.add_argument('--cpu_count', type=int, default=multiprocessing.cpu_count(), help='thread/cpu count')
 
     a = vars(parser.parse_args())
-    pool = multiprocessing.Pool(a["cpu_count"])
+    #pool = multiprocessing.Pool(a["cpu_count"])
 
     os.makedirs(a["index_folder"], exist_ok=True)
 
@@ -56,11 +29,11 @@ def main(_):
     clip = CLIP()
     images = list(glob.glob(os.path.join(a["image_folder"], "*")))
     print(f"""{len(images)} images to handle """)
-    res = list(map(clip.get_image_features, images))
-    #print(res)
-    #print(res[0])
-    #print(len(res))
-    breakpoint()
+    res = list(map(clip.get_image_features, tqdm(images)))
+
+    with open(os.path.join(a["index_folder"], os.path.basename(a["image_folder"]) + ".json"), "w") as xfp:
+        json.dump(res, xfp)
+    print("done")
 
 
 if __name__ == "__main__":
---
+++ query.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+import argparse
+import sys
+import os
+import glob
+import json
+#import multiprocessing
+
+from tqdm import tqdm
+import numpy as np
+import pandas as pd
+from rich import print
+
+from clip_utils import CLIP
+
+
+def transform_json_index(index_json):
+    images = []
+    embeddings = []
+    # TODO: maybe use pandas
+    for x in tqdm(index_json):
+        images.append(x["image"])
+        embeddings.append(np.array(x["features"]))
+    return images, np.array(embeddings).reshape((len(images), -1))
+
+
+def query_index(index_file):
+    print("load index :smiley:")
+    with open(index_file) as xfp:
+        index = json.load(xfp)
+    images, embeddings = transform_json_index(index)
+
+    clip = CLIP()
+    def do_query(query_text):
+        if query_text == "" or not query_text:
+            return
+        query_embedding = clip.get_text_embedding(query_text)
+        res = clip.calc_image_embeddings_text_embedding_similarity(embeddings, query_embedding)
+        df = pd.DataFrame({"image": images, "similarity": res}).sort_values(by="similarity", ascending=False)
+        return df
+    return do_query
+
+
+def main(_):
+    # argument parsing
+    parser = argparse.ArgumentParser(description='query clip index',
+                                     epilog="stg7 2025",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--query", type=str, default=None, help="search query")
+    parser.add_argument("--index_file", type=str, default="index/photos.json", help="index file to load")
+    parser.add_argument("--interactive","-i", action="store_true", help="interactive interface")
+
+    a = vars(parser.parse_args())
+    do_query = query_index(a["index_file"])
+
+    if a["query"]:
+        df = do_query(a["query"])
+
+        print(f"""top 10 matching images to {a["query"]}""")
+        print(df.head(10))
+        print("done")
+        print(df.tail(10))
+
+    if a["interactive"]:
+        print("[bold blue]welcome[/bold blue] :smiley: to the interactive search (empty query to exit)")
+        query = "."
+        while query != "":
+            print("[bold red]prompt>[/bold red] ", end="")
+            query = input()
+            if query != "":
+                df = do_query(query)
+                print(df.head(10))
+                print(":thumbsup:")
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
